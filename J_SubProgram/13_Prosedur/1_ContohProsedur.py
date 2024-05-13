a0ca1de7d7f7259f633eb0e0d75ac1585aea7259
Dede Alamsyah <dedealamsyah29@gmail.com>
2024-05-13 12:46:01 +0700

    new file:   J_SubProgram/13_Prosedur/1_ContohProsedur.py 	new file:   J_SubProgram/13_Prosedur/2_Mendefinisikan_Memanggil-Prosedur.py

---
+++ J_SubProgram/13_Prosedur/1_ContohProsedur.py
@@ -0,0 +1,8 @@
+def greeting(name):
+    print("Halo " + name + ", Selamat Datang!")
+    return
+
+'''Pada contoh di atas, kita membuat fungsi bernama "greeting()" 
+untuk menyapa nama siapa pun yang dikirimkan. 
+'''
+
---
+++ J_SubProgram/13_Prosedur/2_Mendefinisikan_Memanggil-Prosedur.py
@@ -0,0 +1,20 @@
+def greeting(name):
+    print("Halo " + name + ", Selamat Datang!")
+
+greeting("Dede Alamsyah")
+
+"""
+Output: 
+Halo Dede Alamsyah, Selamat Datang!
+"""
+
+#kode dibawah yang tanpa parameter
+def greeting():
+    print("Halo Selamat Datang!")
+
+greeting()
+
+"""
+Output: 
+Halo Selamat Datang!
+"""
