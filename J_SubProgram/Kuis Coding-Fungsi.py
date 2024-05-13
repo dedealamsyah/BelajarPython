4d3da0686944a6bb65f56bd3af0ba176a293a7db
Dede Alamsyah <dedealamsyah29@gmail.com>
2024-05-13 11:29:24 +0700

    new file:   J_SubProgram/Kuis Coding-Fungsi.py

---
+++ J_SubProgram/Kuis Coding-Fungsi.py
@@ -0,0 +1,18 @@
+"""
+TODO:
+Buatlah sebuah fungsi bernama "minimal" dengan ketentuan berikut.
+- Menerima dua buah argumen berupa number, yaitu a dan b.
+- Mengembalikan nilai terkecil antara a atau b.
+- Bila nilai keduanya sama, kembalikan dengan nilai a.
+"""
+
+# TODO: Silakan buat kode Anda di bawah ini.
+def minimal(a, b):
+     return min(a, b)
+
+a = 5
+b = 3
+hasil = minimal(a, b)
+print(f"Nilai terkecil antara {a} dan {b} adalah {hasil}")
+
+  
